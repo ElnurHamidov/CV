2896b20f2660a9fd757366d1f852be0ec28cbdf8
Elnur <gamidovelnur@mail.ru>
2022-12-02 03:01:13 +0400

    created header

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './images/photo.jpg';
+
+class Header extends React.Component{
+
+   
+
+    render(){
+
+        return(
+            <div className="header">
+                <div className="container">
+                    <nav className="nav">
+                    <img src={logo} style={{width: "200px" , height: "200px", borderRadius: '50%'}} className="d-inline-block align-left" alt="my photo" />
+                        <div>
+                            <div className="h2">Elnur Hamidov</div>
+                            <div className="h3">Front end developer</div>
+                        </div>                
+                    </nav>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
